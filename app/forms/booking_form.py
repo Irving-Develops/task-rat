22fb4f5b37e07a5d166ab95214da71ad3ebca1c8
Irving Arreola Palacios <irving.a789@gmail.com>
2022-07-26 23:13:30 +0000

    added form to backend

--- app/forms/booking_form.py
+++ app/forms/booking_form.py
@@ -5,3 +5,5 @@ from wtforms.validators import DataRequired
 
 class BookingForm(FlaskForm):
     completed = BooleanField('completed', vaidators=[DataRequired()])
+    task_id = IntegerField('task_id', vaidators=[DataRequired()])
+    tasker_id = IntegerField('tasker_id', vaidators=[DataRequired()])
