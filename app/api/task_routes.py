a0c16ee3203d05e5ff446668086539d7926bc048
Angie Maidt <angiemaidt@gmail.com>
2022-07-27 17:15:15 -0600

    add tags working, removed comments

--- app/api/task_routes.py
+++ app/api/task_routes.py
@@ -38,17 +38,10 @@ def create_task():
       price = form.data["price"],
       poster_id = form.data["poster_id"],
       danger_level = form.data["danger_level"],
-      # tags = form.data["tags"]
     )
     selected_tags = form.data["tags"]
-    # print(form.data)
-    # print(selected_tags, "SELECTED TAGS!")
     all_tags = Tag.query.all()
-    # # print(all_tags)
-    # print(isinstance(all_tags[0].id, int))
-    # print(isinstance(selected_tags[0], int))
     added_tags = [tag for tag in all_tags for selected_tag in selected_tags if tag.id == int(selected_tag)]
-    # print(added_tags, 'this is the added tags')
     task.tags = added_tags
 
     db.session.add(task)
