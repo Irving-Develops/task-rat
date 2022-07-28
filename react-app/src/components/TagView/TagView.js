427457b9056c49797d986fe03b1a555c35c978b9
Angie Maidt <angiemaidt@gmail.com>
2022-07-28 11:07:30 -0600

    getting pageTag

--- react-app/src/components/TagView/TagView.js
+++ react-app/src/components/TagView/TagView.js
@@ -1,4 +1,31 @@
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import TaskCard from '../Tasks/taskCard/taskCard'
+
 function TagView() {
+    const [tags, setTags] = useState('')
+    const { id } = useParams()
+
+    let pageTag
+    if (tags) {
+        // console.log(Object.values(tags))
+        pageTag = Object.values(tags).filter(tag => tag.id === Number(id))
+    }
+
+    console.log(pageTag)
+
+
+    useEffect(() => {
+        const fetchTags = async () => {
+            const res = await fetch('/api/tasks/tags')
+            const data = await res.json()
+            // console.log(data)
+            setTags(data.tags)
+        }
+        fetchTags()
+    }, [])
+
     return (
         <h1>Greetings from Tags!</h1>
     )
