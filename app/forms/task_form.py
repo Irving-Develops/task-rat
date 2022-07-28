4a8cf661c993800e48266b3266bf71112747c529
Wesley Blackburn <wesley.blackburn90@yahoo.com>
2022-07-28 12:30:59 -0400

    task form validators

--- app/forms/task_form.py
+++ app/forms/task_form.py
@@ -1,15 +1,13 @@
 from wsgiref.validate import validator
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, TextAreaField, SelectField, BooleanField, RadioField, FieldList, SelectMultipleField
-from wtforms.validators import DataRequired
-from ..models import Tag
-
+from wtforms import StringField, IntegerField, TextAreaField, SelectField, BooleanField, SelectMultipleField
+from wtforms.validators import DataRequired, Length
 class TaskForm(FlaskForm):
-  title = StringField('Title', validators=[DataRequired()])
-  description = TextAreaField('Description', validators=[DataRequired()])
-  city = StringField('City', validators=[DataRequired()])
-  state = StringField('State', validators=[DataRequired()])
-  country = StringField('Country', validators=[DataRequired()])
+  title = StringField('Title', validators=[DataRequired(), Length(min=5, max=50)], )
+  description = TextAreaField('Description', validators=[DataRequired(), Length(min=5, max=500)])
+  city = StringField('City', validators=[DataRequired(), Length(max=30)])
+  state = StringField('State', validators=[DataRequired(), Length(max=30)])
+  country = StringField('Country', validators=[DataRequired(), Length(max=30)])
   price = IntegerField('Price', validators=[DataRequired()])
   poster_id = IntegerField("poster_id", validators=[DataRequired()])
   danger_level = SelectField("Danger Level", validators=[DataRequired()], choices=[("1"), ("2"), ("3"), ("4"), ("5")])
