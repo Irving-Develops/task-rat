1c08fe84f02b94b31792a78a74c89b7aa6ed8423
Angie Maidt <angiemaidt@gmail.com>
2022-07-28 16:43:21 -0600

    edit form modal done

--- react-app/src/components/Tasks/editTaskForm/editTaskForm.js
+++ react-app/src/components/Tasks/editTaskForm/editTaskForm.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { useHistory } from "react-router-dom"
 import { editTaskThunk, getTasksThunk } from "../../../store/tasks"
+import './editTaskForm.css'
 
 const EditTaskForm = ({ task, setShowEditForm, showEditForm }) => {
   const dispatch = useDispatch()
@@ -74,7 +75,6 @@ const EditTaskForm = ({ task, setShowEditForm, showEditForm }) => {
       tags
     }
 
-    setErrors([])
     setShowEditForm(!showEditForm)
 
     try {
@@ -89,145 +89,152 @@ const EditTaskForm = ({ task, setShowEditForm, showEditForm }) => {
   }
 
   return (
-    <section>
-      <form onSubmit={handleSubmit}>
-        {errors.length > 0 && (
-          <div className='errors-container'>
-              The following errors were found:
-              <ul className='errors'>
-                  {errors.map(error => (
-                      <li className='error' key={error}>{error}</li>
-                  ))}
-              </ul>
-          </div>
-        )}
-        <input
-          type="text"
-          placeholder="Title"
-          required
-          value={title}
-          onChange={updateTitle} />
-        <input
-          type="text"
-          placeholder="Description"
-          required
-          value={description}
-          onChange={updateDescription} />
-        <input
-          type="text"
-          placeholder="City"
-          required
-          value={city}
-          onChange={updateCity} />
-        <input
-          type="text"
-          placeholder="State"
-          required
-          value={state}
-          onChange={updateState} />
-        <input
-          type="text"
-          placeholder="Country"
-          required
-          value={country}
-          onChange={updateCountry} />
-        <input
-          type="text"
-          placeholder="Price"
-          required
-          value={price}
-          onChange={updatePrice} />
-        <select onChange={updateDangerLevel}>
-          <option value="1"> 1 </option>
-          <option value="2"> 2 </option>
-          <option value="3"> 3 </option>
-          <option value="4"> 4 </option>
-          <option value="5"> 5 </option>
-        </select>
-        <label>Guns</label>
-        <input
-        type='checkbox'
-        name='guns'
-        value='1'
-        // checked={tags.includes("1")}
-        onChange={(e) => updateTags(e)}
-        ></input>
-        <label>Explosives</label>
-        <input
-        type='checkbox'
-        name='explosives'
-        value='2'
-        // checked={tags.includes("2")}
-        onChange={(e) => updateTags(e)}
-        ></input>
-        <label>Stealth</label>
-        <input
-        type='checkbox'
-        name='stealth'
-        value='3'
-        onChange={(e) => updateTags(e)}
-        // checked={tags.includes("3")}
-        ></input>
-        <label>Survival</label>
-        <input
-        type='checkbox'
-        name='survival'
-        value='4'
-        onChange={(e) => updateTags(e)}
-        // checked={tags.includes("4")}
-        ></input>
-        <label>Medicine</label>
-        <input
-        type='checkbox'
-        name='medicine'
-        value='5'
-        onChange={(e) => updateTags(e)}
-        // checked={tags.includes("5")}
-        ></input>
-        <label>Repairs</label>
-        <input
-        type='checkbox'
-        name='repairs'
-        value='6'
-        onChange={(e) => updateTags(e)}
-        // checked={tags.includes("6")}
-        ></input>
-        <label>Pilot</label>
-        <input
-        type='checkbox'
-        name='pilot'
-        value='7'
-        onChange={(e) => updateTags(e)}
-        // checked={tags.includes("7")}
-        ></input>
-        <label>Hacking</label>
-        <input
-        type='checkbox'
-        name='hacking'
-        value='8'
-        onChange={(e) => updateTags(e)}
-        // checked={tags.includes("8")}
-        ></input>
-        <label>Hand-to-Hand</label>
-        <input
-        type='checkbox'
-        name='hand-to-hand'
-        value='9'
-        onChange={(e) => updateTags(e)}
-        // checked={tags.includes("9")}
-        ></input>
-        <label>Charisma</label>
-        <input
-        type='checkbox'
-        name='charisma'
-        value='10'
-        onChange={(e) => updateTags(e)}
-        // checked={tags.includes("10")}
-        ></input>
-        <button type="submit" id="taskFormSubmitButton"> Submit your task </button>
-        <button onClick={() => setShowEditForm(!showEditForm)}>Cancel</button>
-      </form>
-    </section>
+    <>
+        <div className='background-blocker'
+          onClick={() => setShowEditForm(!showEditForm)}
+        ></div>
+        <div id="edit-form-container">
+        <form onSubmit={handleSubmit}>
+          {errors.length > 0 && (
+            <div className='errors-container'>
+                The following errors were found:
+                <ul className='errors'>
+                    {errors.map(error => (
+                        <li className='error' key={error}>{error}</li>
+                    ))}
+                </ul>
+            </div>
+          )}
+          <input
+            type="text"
+            placeholder="Title"
+            required
+            value={title}
+            onChange={updateTitle} />
+          <input
+            type="text"
+            placeholder="Description"
+            required
+            value={description}
+            onChange={updateDescription} />
+          <input
+            type="text"
+            placeholder="City"
+            required
+            value={city}
+            onChange={updateCity} />
+          <input
+            type="text"
+            placeholder="State"
+            required
+            value={state}
+            onChange={updateState} />
+          <input
+            type="text"
+            placeholder="Country"
+            required
+            value={country}
+            onChange={updateCountry} />
+          <input
+            type="text"
+            placeholder="Price"
+            required
+            value={price}
+            onChange={updatePrice} />
+          <select onChange={updateDangerLevel}>
+            <option value="1"> 1 </option>
+            <option value="2"> 2 </option>
+            <option value="3"> 3 </option>
+            <option value="4"> 4 </option>
+            <option value="5"> 5 </option>
+          </select>
+          <label>Guns</label>
+          <input
+          type='checkbox'
+          name='guns'
+          value='1'
+          // checked={tags.includes("1")}
+          onChange={(e) => updateTags(e)}
+          ></input>
+          <label>Explosives</label>
+          <input
+          type='checkbox'
+          name='explosives'
+          value='2'
+          // checked={tags.includes("2")}
+          onChange={(e) => updateTags(e)}
+          ></input>
+          <label>Stealth</label>
+          <input
+          type='checkbox'
+          name='stealth'
+          value='3'
+          onChange={(e) => updateTags(e)}
+          // checked={tags.includes("3")}
+          ></input>
+          <label>Survival</label>
+          <input
+          type='checkbox'
+          name='survival'
+          value='4'
+          onChange={(e) => updateTags(e)}
+          // checked={tags.includes("4")}
+          ></input>
+          <label>Medicine</label>
+          <input
+          type='checkbox'
+          name='medicine'
+          value='5'
+          onChange={(e) => updateTags(e)}
+          // checked={tags.includes("5")}
+          ></input>
+          <label>Repairs</label>
+          <input
+          type='checkbox'
+          name='repairs'
+          value='6'
+          onChange={(e) => updateTags(e)}
+          // checked={tags.includes("6")}
+          ></input>
+          <label>Pilot</label>
+          <input
+          type='checkbox'
+          name='pilot'
+          value='7'
+          onChange={(e) => updateTags(e)}
+          // checked={tags.includes("7")}
+          ></input>
+          <label>Hacking</label>
+          <input
+          type='checkbox'
+          name='hacking'
+          value='8'
+          onChange={(e) => updateTags(e)}
+          // checked={tags.includes("8")}
+          ></input>
+          <label>Hand-to-Hand</label>
+          <input
+          type='checkbox'
+          name='hand-to-hand'
+          value='9'
+          onChange={(e) => updateTags(e)}
+          // checked={tags.includes("9")}
+          ></input>
+          <label>Charisma</label>
+          <input
+          type='checkbox'
+          name='charisma'
+          value='10'
+          onChange={(e) => updateTags(e)}
+          // checked={tags.includes("10")}
+          ></input>
+          <button type="submit" id="taskFormSubmitButton"> Submit Edit</button>
+          <button onClick={() => setShowEditForm(!showEditForm)}>Cancel</button>
+        </form>
+      </div>
+    </>
+
+
   )
 }
 
