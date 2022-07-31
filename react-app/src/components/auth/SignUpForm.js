b52ac678b8e4b536c71bdd27e3c9d4f7a0c413d9
Angie Maidt <angiemaidt@gmail.com>
2022-07-30 22:16:51 -0600

    editing signup form

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -65,8 +65,8 @@ const SignUpForm = () => {
           <div className='vaultboy-bg'>
             <img src='https://www.pngkey.com/png/full/152-1529343_fallout-3-vault-boy-png-picture-download-fallout.png' alt="vaultboy"/>
           </div>
-          <div class="bubble bubble-bottom-left" contenteditable>
-            {hasSubmitted && errors.length > 0 ? (
+          <div className="bubble bubble-bottom-left" contenteditable>
+            {errors.length > 0 ? (
               <ul className='errors'>
                 {errors.map(error => (
                     <li className='error' key={error}>{error}</li>
@@ -88,37 +88,41 @@ const SignUpForm = () => {
               </div>
               <div className='input-wrapper'>
                 <div>
-                  <label>User Name</label>
+                  <h4>User Name</h4>
                   <input
                     type='text'
                     name='username'
+                    placeholder='Username'
                     onChange={updateUsername}
                     value={username}
                   ></input>
                 </div>
                 <div>
-                  <label>Email</label>
+                  <h4>Email</h4>
                   <input
                     type='text'
                     name='email'
+                    placeholder='Email'
                     onChange={updateEmail}
                     value={email}
                   ></input>
                 </div>
                 <div>
-                  <label>Password</label>
+                  <h4>Password</h4>
                   <input
                     type='password'
                     name='password'
+                    placeholder='Password'
                     onChange={updatePassword}
                     value={password}
                   ></input>
                 </div>
                 <div>
-                  <label>Repeat Password</label>
+                  <h4>Repeat Password</h4>
                   <input
                     type='password'
                     name='repeat_password'
+                    placeholder='Confirm Password'
                     onChange={updateRepeatPassword}
                     value={repeatPassword}
                     required={true}
@@ -146,34 +150,37 @@ const SignUpForm = () => {
         <div className='input-container'>
           <div>
             <div className='input-headers'>
-              <h2>Sweet, now why don't you tell us a little about yourself?</h2>
+              <h2>Now, why don't you tell us a little about yourself?</h2>
               <h4>This information will be displayed on your user profile page. Don't worry, you can edit it later.</h4>
             </div>
             <div className='input-wrapper'>
               <div>
-                <label>First Name</label>
+                <h4>First Name</h4>
                 <input
                   type='text'
                   name='first_name'
+                  placeholder='First Name'
                   onChange={(e) => setFirstName(e.target.value)}
                   value={first_name}
                 ></input>
               </div>
               <div>
-                <label>Last Name</label>
+                <h4>Last Name</h4>
                 <input
                   type='text'
                   name='last_name'
+                  placeholder='Last Name'
                   onChange={(e) => setLastName(e.target.value)}
                   value={last_name}
                 ></input>
               </div>
               <div>
-                <label>Bio</label>
+                <h4>Bio</h4>
                 <textarea
                   type='text'
                   name='bio'
-                  onChange={(e) => setBio(e.target.value)}
+                  placeholder='Share your skills, likes, dislikes, what three items you would take into the wasteland... get creative!'
+                  onChange={updateBio}
                   value={bio}
                 ></textarea>
               </div>
@@ -195,7 +202,6 @@ const SignUpForm = () => {
           </div>
         </div>
         }
-
         {count === 3 &&
           <div className='input-container'>
             <div>
@@ -205,28 +211,31 @@ const SignUpForm = () => {
               </div>
               <div className='input-wrapper'>
                 <div>
-                  <label>City</label>
+                  <h4>City</h4>
                   <input
                     type='text'
                     name='city'
+                    placeholder='City'
                     onChange={(e) => setCity(e.target.value)}
                     value={city}
                   ></input>
                 </div>
                 <div>
-                  <label>State</label>
+                  <h4>State</h4>
                   <input
                     type='text'
                     name='state'
+                    placeholder='State'
                     onChange={(e) => setState(e.target.value)}
                     value={state}
                   ></input>
                 </div>
                 <div>
-                  <label>Country</label>
+                  <h4>Country</h4>
                   <input
                     type='text'
                     name='country'
+                    placeholder='Country'
                     onChange={(e) => setCountry(e.target.value)}
                     value={country}
                   ></input>
@@ -255,10 +264,10 @@ const SignUpForm = () => {
             <div>
               <div className='input-headers'>
                 <h2>Finally, choose an avatar.</h2>
-                <h4>How do you want to present yourself to the world?</h4>
+                <h4>Choose the one that speaks to your soul.</h4>
               </div>
               <div className='input-wrapper'>
-                  <label>Pic Url</label>
+                  <h4>Pic Url</h4>
                   <input
                     type='radio'
                     name='pic_url'
@@ -297,10 +306,60 @@ const SignUpForm = () => {
                   disabled={count < 2}
                   className='task-form-buttons'
                 >Back</button>
-                <button type='submit'>Sign Up</button>
+                <button
+                  type='button'
+                  onClick={() => setCount(count + 1)}
+                  disabled={count > 4}
+                  className='task-form-buttons'
+                >Next</button>
               </div>
             </div>
           </div>
+        }
+        {count === 5 &&
+          <div className='input-container final-screen'>
+          <div className='final-screen-wrapper'>
+            <div className="input-headers">
+              <h2>Everything look correct?</h2>
+              <h4>Click on a section to edit.</h4>
+            </div>
+            <div className="final-screen-content-wrapper">
+              <div onClick={() => setCount(1)} className='new-title-content final-screen-content'>
+                <h5>Username: </h5>
+                <p>{username}</p>
+              </div>
+              <div onClick={() => setCount(1)} className='new-description-content final-screen-content'>
+                <h5>Email: </h5>
+                <p>{email}</p>
+              </div>
+              <div onClick={() => setCount(2)} className='new-location-content final-screen-content'>
+                <h5>Name: </h5>
+                <p>{first_name} {last_name}</p>
+              </div>
+              <div onClick={() => setCount(2)} className='new-description-content final-screen-content'>
+                <h5>Bio: </h5>
+                <p>{bio}</p>
+              </div>
+              <div onClick={() => setCount(4)} className='new-price-content final-screen-content'>
+              <h5>Location: </h5>
+                <p>{city}, {state}, {country}</p>
+              </div>
+              <div onClick={() => setCount(4)} className='new-danger-level-content final-screen-content'>
+                <h5>Avatar: </h5>
+                <img src={pic_url} alt='user-pic' />
+              </div>
+            </div>
+          </div>
+          <div className='task-button-container'>
+            <button
+              type='button'
+              onClick={() => setCount(count - 1)}
+              disabled={count < 2}
+              className='task-form-buttons'
+            >Back</button>
+            <button type="submit" className='task-form-buttons'>Sign Up!</button>
+          </div>
+        </div>
         }
         </form>
       </section>
