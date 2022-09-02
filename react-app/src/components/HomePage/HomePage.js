5f5008b491b582b0e9528f222a4a109f6a969ca1
Angie Maidt <angiemaidt@gmail.com>
2022-09-02 16:10:39 -0600

    fixed up user images

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -79,58 +79,7 @@ function HomePage() {
                     </div>
                 </div>
             </div>
-
-            {/* <Footer/> */}
-
-            {/* <div className="footer">
-                <div id="team">
-                    <div id="entire-team">
-                        <div id="footer-content" className="about-me">
-                            <div id="person">
-                                <div id="name">
-                                    Irving Arreola
-                                </div>
-                                <div id="links">
-                                    <a href="https://github.com/Irving-Develops"><img className='user-card-img' src="https://t2marketinginternational.com/wp-content/uploads/2018/06/Github-Logo-450x450.png" alt="github logo"/></a>
-                                    <a href="https://www.linkedin.com/in/irving-arreola-palacios-5bb10414a/"><img className="user-card-img" src="https://www.maryville.edu/wp-content/uploads/2015/11/Linkedin-logo-1-550x550-300x300.png" alt="linked-in logo"/></a>
-                                </div>
-                            </div>
-                        </div>
-                        <div id="footer-content" className="about-me">
-                            <div id="person">
-                                <div id="name">
-                                    Wesley Blackburn
-                                </div>
-                                <div id="links">
-                                    <a href="https://github.com/wesleyblackburn90"><img className="user-card-img" src="https://t2marketinginternational.com/wp-content/uploads/2018/06/Github-Logo-450x450.png" alt="github logo"/></a>
-                                    <a href="https://www.linkedin.com/in/jay-hutts-300ab9180/"><img className="user-card-img" src="https://www.maryville.edu/wp-content/uploads/2015/11/Linkedin-logo-1-550x550-300x300.png" alt="linked-in logo"/></a>
-                                </div>
-                            </div>
-                        </div>
-                        <div id="footer-content" className="about-me">
-                            <div id="person">
-                                <div id="name">
-                                    Jay Hutts
-                                </div>
-                                <div id="links">
-                                    <a href="https://github.com/jay-bean"><img className="user-card-img" src="https://t2marketinginternational.com/wp-content/uploads/2018/06/Github-Logo-450x450.png" alt="github logo"/></a>
-                                    <a href="https://www.linkedin.com/in/jay-hutts-300ab9180/"><img className="user-card-img" src="https://www.maryville.edu/wp-content/uploads/2015/11/Linkedin-logo-1-550x550-300x300.png" alt="linked-in logo"/></a>
-                                </div>
-                            </div>
-                        </div>
-                        <div id="footer-content" className="about-me">
-                            <div id="person">
-                                <div id="name">
-                                    Angie Maidt
-                                </div>
-                                <div id="links">
-                                    <a href="https://github.com/angMaidt"><img className="user-card-img" src="https://t2marketinginternational.com/wp-content/uploads/2018/06/Github-Logo-450x450.png" alt="github logo"/></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
+            <h2 id='thanks'>Thanks for checking us out!</h2>
         </>
     )
 }
